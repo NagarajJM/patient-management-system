fd6a692b26638c5c4087964a958e70d67aaa086d
NagarajJM <nagarajjm@yahoo.com>
2025-10-15 14:04:48 +0530

    Initial commit

---
+++ integration-tests/src/test/java/AuthIntegrationTest.java
@@ -0,0 +1,42 @@
+import static org.hamcrest.CoreMatchers.notNullValue;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+
+public class AuthIntegrationTest {
+
+	@BeforeAll
+	static void setUp() {
+		RestAssured.baseURI = "http://localhost:4004";
+	}
+
+	@Test
+	public void shouldReturnOKWithValidToken() {
+		String loginPayload = """
+					{
+						"email": "testuser@test.com",
+						"password": "password123"
+					}
+				""";
+
+		Response response = RestAssured.given().contentType("application/json").body(loginPayload).when()
+				.post("auth/login").then().statusCode(200).body("token", notNullValue()).extract().response();
+
+		System.out.println("Generated Token: " + response.jsonPath().getString("token"));
+	}
+
+	@Test
+	public void shouldReturnUnauthorizedOnInvalidLogin() {
+		String loginPayload = """
+					{
+						"email": "invalid_user@test.com",
+						"password": "wrongpassword"
+					}
+				""";
+
+		RestAssured.given().contentType("application/json").body(loginPayload).when().post("auth/login").then().statusCode(401);
+	}
+}
